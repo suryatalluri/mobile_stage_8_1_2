80f5ee409ecd3c2ec9efa152a40cc515eb757e8e
Surya <surya.talluri@gmail.com>
2016-04-19 12:49:17 +0000

    New files added - [demo-options.json,prefabPropertiesMetadata.json,ng-map.min.js,favicon.ico,Page.css,servicedef.xml,Main.css,app.css,Main.css,general-options.json,page.min.html,.wmproject.properties,pages-config.json,app.css,add.jar,Main.js,types.js,index.html,prefabProperties.properties,prefab-dispatcher-servlet.xml,app.variables.json,page.min.html,Readme.txt,en.json,securityService.spring.xml,prefab-icon.png,page.min.html,intercept-urls.json,Main.html,prefabPropertiesMetadata.json,.wmproject.properties,Readme.txt,prefab-dispatcher-servlet.xml,app.variables.json,Readme.txt,spinner-small.gif,wmProperties.js,Readme.txt,Page.variables.json,Main.html,Page.js,Main.js,config.json,project-rest-service.xml,app.js,app.js,index.html,en.json,Readme.txt,Readme.txt,default-image.png,types.js,Readme.txt,log4j.properties,Page.html,Readme.txt,.wmproject.properties,roles.json,prefab-icon.png,project-rest-service.xml,favicon.ico,Main.variables.json,AdditionProg.spring.xml,Main.variables.json,googlemaps-prefab-services.xml,config.json,googlemaps.jar,securityService.spring.xml,add-prefab-services.xml,prefabProperties.properties,Page] Files modified - [pages-config.json,project-services.xml,FileController.java,FileService.java,project-security-provider.xml,hrdb.properties,app.variables.json] Refactored files - [] Fixed bugs - [] Removed files - []

--- services/FileService/src/com/mobile_stage_8_1_2/fileservice/FileService.java
+++ services/FileService/src/com/mobile_stage_8_1_2/fileservice/FileService.java
@@ -6,6 +6,7 @@ package com.mobile_stage_8_1_2.fileservice;
 
 import com.wavemaker.runtime.WMAppContext;
 import com.wavemaker.runtime.file.model.DownloadResponse;
+
 import com.wavemaker.runtime.file.manager.FileServiceManager;
 import com.wavemaker.runtime.util.WMRuntimeUtils;
 
---
+++ src/main/webapp/app/prefabs/add/wmProperties.js
@@ -0,0 +1,10 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "defaultLanguage" : "en",
+  "displayName" : "add",
+  "homePage" : "Main",
+  "name" : "add",
+  "platformType" : "DEFAULT",
+  "type" : "PREFAB",
+  "version" : "1.0"
+};
